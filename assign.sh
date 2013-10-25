93ac6faccf237f8ae4e7ea11e323fc5e8456ad05
Darron Froese <darron@froese.org>
2013-10-25 09:40:46 -0600

    Change a targeted server's role.

--- assign.sh
+++ assign.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 # Assign role to server.
 PAYLOAD=$(cat)
-echo "Assign to $PAYLOAD role."
-echo "My role is $SERF_SELF_ROLE"
+TARGET=$(echo $PAYLOAD | cut -f1 -d,)
+ROLE=$(echo $PAYLOAD | cut -f2 -d,)
+
+echo "Assign $TARGET to $ROLE role."
+echo "$SERF_SELF_NAME: My current role is $SERF_SELF_ROLE"
+
+if [ $SERF_SELF_NAME == $TARGET ]
+then
+  echo "ROLE=$ROLE" > /etc/serf/role
+  echo "Changing $SERF_SELF_NAME to $ROLE"
+  service serf reload
+else
+  echo "Not changing $SERF_SELF_NAME's role."
+fi
