6e809070c12c9caf03c2c2c3443a0476e5ba33af
Darron Froese <darron@froese.org>
2013-10-27 16:26:47 -0600

    Hmm - build works on command linke but not via Serf.

--- build.sh
+++ build.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 # Build Docker container based on git repo with Dockerfile.
+# Ex: "serf event build http://git.repo.goes.here,image"
 PAYLOAD=$(cat)
-echo "Build: $PAYLOAD"
+REPO=$(echo $PAYLOAD | cut -f1 -d,)
+IMAGE=$(echo $PAYLOAD | cut -f2 -d,)
+BUILD_DIR=`mktemp -d`
+
+echo "Build: $REPO as $IMAGE in $BUILD_DIR"
 echo "My role is $SERF_SELF_ROLE"
+
+git clone $REPO $BUILD_DIR
+echo "/usr/bin/docker build -t nonfiction/$IMAGE $BUILD_DIR"
+/usr/bin/docker build -t nonfiction/$IMAGE $BUILD_DIR
 
 # Tag
 
