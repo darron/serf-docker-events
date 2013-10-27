66bee9f7f2839658e6f16c33b68d6d4cd3156557
Darron Froese <darron@froese.org>
2013-10-27 16:36:29 -0600

    Stop an image with an ID.

--- stop.sh
+++ stop.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Stop Docker container based on docker image.
-PAYLOAD=$(cat)
-echo "Stop: $PAYLOAD"
+# Ex: serf events stop 6edec2ef1343
+ID=$(cat)
+echo "Stop: $ID"
 echo "My role is $SERF_SELF_ROLE"
+
+/usr/bin/docker stop $ID
