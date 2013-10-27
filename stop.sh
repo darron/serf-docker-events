f1c7288b803a4962c3878f80c6e53027f74c28ed
Darron Froese <darron@froese.org>
2013-10-27 16:39:52 -0600

    Typo.

--- stop.sh
+++ stop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Stop Docker container based on docker image.
-# Ex: serf events stop 6edec2ef1343
+# Ex: serf event stop 6edec2ef1343
 ID=$(cat)
 echo "Stop: $ID"
 echo "My role is $SERF_SELF_ROLE"
