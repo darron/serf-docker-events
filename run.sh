c398e9d058fb3809263d9e0e8102d73f66b58e5d
Darron Froese <darron@froese.org>
2013-10-28 16:58:48 -0600

    Move into role check.

--- run.sh
+++ run.sh
@@ -5,4 +5,10 @@ IMAGE=$(cat)
 echo "Run: $IMAGE"
 echo "My role is $SERF_SELF_ROLE"
 
-/usr/bin/docker run -d $IMAGE
+if [ $SERF_SELF_ROLE == 'serve' ]
+then
+  /usr/bin/docker run -d $IMAGE
+  # TODO - get the port number.
+else
+fi
+
